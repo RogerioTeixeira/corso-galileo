7722e3324ea96021d54849204fd5cc62e1de2fd1
Teixeira Nunes Rogerio <TeixeiraR@corvallis.it>
2019-11-28 11:33:18 +0100

    update project

--- src/app/cliente/cliente.component.ts
+++ src/app/cliente/cliente.component.ts
@@ -4,6 +4,7 @@ import { ClienteService } from '../services/cliente.service';
 import { Router } from '@angular/router';
 import { ApiService } from '../services/api.service';
 import { HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-cliente',
@@ -13,12 +14,39 @@ import { HttpParams } from '@angular/common/http';
 export class ClienteComponent implements OnInit {
   public clienti: Array<Customer> = new Array<Customer>();
   public filter: string;
+  public cliente: any;
+  public obs: Observable<string>;
   constructor(public clienteService: ClienteService, private router: Router) { }
 
   ngOnInit() {
     this.clienteService.getAll(true).subscribe((clienti) => {
       this.clienti = clienti;
     })
+
+    /* this.obs = new Observable((observer) => {
+      setTimeout(() => {
+        observer.next('ciao');
+        observer.complete();
+      }, 5000)
+    })
+
+
+    this.obs.subscribe((value: string) => {
+      console.log("Risposta callback");
+    })
+
+    console.log("fuori callback");
+    this.cliente = {
+      id: 2,
+      city: "milano",
+      codeIva: 12244,
+      address: "dddd",
+      country: "Italia",
+      name: "Google",
+      invoices: [],
+      urlLogo: "http://xxxxx.png",
+      calcola: (val1, val2) => { return val1 + val2 }
+    } */
 
 
   }
@@ -33,7 +61,7 @@ export class ClienteComponent implements OnInit {
   }
 
   select(cliente: Customer) {
-    this.router.navigate(['cliente', cliente.id]);
+    this.router.navigate(['pages/cliente', cliente.id]);
   }
 
 }
